20fae3fd72edf171282aec1ecf9cbf3f2622aa7c
kamil10x <shaik.kamil10@gmail.com>
2026-06-14 22:48:52 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,34 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
+
+//const express = require('express');
 
 const app = express();
+const PORT = 4000;
 
-require("./conn");
-
+require('./conn');
 app.use(express.json());
 
 app.use(cors({
-    credentials: true,
-    origin: [
-        "http://localhost:5173",
-        "https://your-frontend-name.vercel.app"
-    ]
-}));
+    credentials:true,
+    origin:"http://localhost:5173"
+}))
 
-const userRoutes = require("./Routes/user");
-const ResumeRoutes = require("./Routes/resume");
+const userRoutes = require('./Routes/user');
+const ResumeRoutes = require('./Routes/resume');
 
-app.use("/api/user", userRoutes);
-app.use("/api/resume", ResumeRoutes);
 
-const PORT = process.env.PORT || 4000;
+app.use('/api/user', userRoutes)
+app.use('/api/resume', ResumeRoutes)
+/*
+app.get('/', (req, res)=>{
+    res.send({
+        message:"Hi welcome kamil to backend"
+    })
+})*/
 
-app.listen(PORT, () => {
-    console.log(`Backend is running on port ${PORT}`);
-});
+app.listen(PORT, ()=>{
+    console.log("Backend is running on port ", PORT);
+})
