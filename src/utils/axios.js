e46c447c595db196c0ff8869573b07730860dfed
kamil10x <shaik.kamil10@gmail.com>
2026-06-14 23:22:31 +0530

    backend deployed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL:"https://rusume-ai-scorer-1.onrender.com",
     //timeout:1000,
    // headers:{'Content-Type':'application/json'}
 })
